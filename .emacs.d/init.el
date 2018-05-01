b271c45f3ca42e56455dfd27ca480b10e6e1bd7c
Mark Bomford <bomford@gmail.com>
2018-05-01 11:10:30 -0400

    that was quite the .gitignore cleanup

---
+++ .emacs.d/init.el
@@ -0,0 +1,30 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; 2018-04-28 mjb attempting to use orgfile for init (seems to work)
+
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "settings.org"
+                   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (ox-pandoc html5-schema org-edna))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; 2018-04-29 mjb attempting to add repos
+
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")))
---
+++ .emacs.d/settings.el
@@ -0,0 +1,6 @@
+(menu-bar-mode -1)
+
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
+(set-default 'truncate-lines nil)
