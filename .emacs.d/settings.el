30dca9538a24ac5c62607b87a909ba41e1f8daa3
mb982 <mark.bomford@yale.edu>
2018-05-08 15:47:51 -0400

    emacs likes mouse

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -1,5 +1,32 @@
 (menu-bar-mode -1)
 
+(xterm-mouse-mode)
+(require 'mouse)
+(xterm-mouse-mode t)
+(defun track-mouse (e))
+
+(setq mouse-wheel-follow-mouse 't)
+
+(defvar alternating-scroll-down-next t)
+(defvar alternating-scroll-up-next t)
+
+(defun alternating-scroll-down-line ()
+  (interactive "@")
+    (when alternating-scroll-down-next
+;      (run-hook-with-args 'window-scroll-functions )
+      (scroll-down-line))
+    (setq alternating-scroll-down-next (not alternating-scroll-down-next)))
+
+(defun alternating-scroll-up-line ()
+  (interactive "@")
+    (when alternating-scroll-up-next
+;      (run-hook-with-args 'window-scroll-functions)
+      (scroll-up-line))
+    (setq alternating-scroll-up-next (not alternating-scroll-up-next)))
+
+(global-set-key (kbd "<mouse-4>") 'alternating-scroll-down-line)
+(global-set-key (kbd "<mouse-5>") 'alternating-scroll-up-line)
+
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
 
